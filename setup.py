47dc59ac5c8234f9bc4b0953826b6516274407ab
Lev.Veydenbah <Lev.Veydenbah@nexign-systems.com>
2019-10-10 13:15:45 +0400

    version 2.0.0 discontinued support for python < 3.5 added type annotations 'f-strings' usage bugfixes

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='robotframework-postgresqldb',
-    version='1.0.2',
+    version='2.0.0',
     description='Robot Framework Library For Working With PostgreSQL Database.',
     long_description=long_description,
     url='https://github.com/peterservice-rnd/robotframework-postgresqldb',
@@ -26,7 +26,6 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Testing',
         'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Framework :: Robot Framework :: Library',
